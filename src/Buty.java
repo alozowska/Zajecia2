65a4ceb32005943297e305cab1bc3db20583075e
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-07-08 16:20:12 +0200

    Rozwiązanie 1.2 (dodanie konstruktora)

--- src/Buty.java
+++ src/Buty.java
@@ -2,5 +2,9 @@ public class Buty {
     String marka;
     String kolor;
     int obcas;
-
+Buty(String m, String k, int o){
+    marka= m;
+    kolor= k;
+    obcas= o;
+}
 }
--- src/Kolekcja.java
+++ src/Kolekcja.java
@@ -1,15 +1,7 @@
 public class Kolekcja {
     public static void main(String[] args) {
-        Buty buty1= new Buty();
-        Buty buty2= new Buty();
-
-        buty1.marka= "badura";
-        buty1.kolor= "czarny";
-        buty1.obcas= 10;
-
-        buty2.marka= "venezia";
-        buty2.kolor= "czerwony";
-        buty2.obcas= 8;
+        Buty buty1= new Buty("badura", "czarny", 10);
+        Buty buty2= new Buty("venezia", "czerwony", 8);
 
         System.out.println(buty1.marka+" "+buty1.kolor+" "+ buty1.obcas+"cm");
         System.out.println(buty2.marka+" "+buty2.kolor+" "+buty2.obcas+"cm");
